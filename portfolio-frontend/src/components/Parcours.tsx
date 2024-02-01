396c280b51eb613c1c053f040f0b520b1e103ebf
Coco <coulet.corentin@gmail.com>
2024-02-01 17:18:17 +0100

    configuration de parcours

--- portfolio-frontend/src/components/Parcours.tsx
+++ portfolio-frontend/src/components/Parcours.tsx
@@ -1,11 +1,46 @@
 import { Chrono } from "react-chrono";
 import DuAccess from "../assets/DU-AccES.mp4";
+import "../styles/parcours.scss";
 
 const Parcours = () => {
 
   const items = [
-    
-    
+    {
+      title: "2023 - 2024",
+      cardTitle: "Développeur Full Stack en alternance",
+      cardSubtitle:"3WA Academy",
+      cardDetailedText: "Cette formation développeur full-stack en alternance prépare au métier de développeur full-stack, c'est-à-dire être capable de concevoir et réaliser de A à Z un site ou une application web et mobile, de comprendre et de travailler sur “toute la slack”.",
+      media: {   
+        type: "IMAGE",
+        source: {
+          url: "https://iutp.univ-poitiers.fr/geii/wp-content/uploads/sites/21/2014/03/Logo-GEII-fond-transparent-HR.gif",
+        }
+      }
+    },
+    {
+      title: "2022 - 2023",
+      cardTitle: "Réparateur Informatique",
+      cardSubtitle:"Econocom Factory",
+      cardDetailedText: "Mon premier travail dans lequel j'ai pu me faire une expérience dans le monde professionnel. En charge de la réparation hardware et software d'appareils électroniques (ordinateurs, téléphones etc)",
+      media: {   
+        type: "IMAGE",
+        source: {
+          url: "https://iutp.univ-poitiers.fr/geii/wp-content/uploads/sites/21/2014/03/Logo-GEII-fond-transparent-HR.gif",
+        }
+      }
+    },
+    {
+      title: "2021 - 2022",
+      cardTitle: "BUT Informatique 1ère année",
+      cardSubtitle:"Université Montpellier-Sète",
+      cardDetailedText: "Le B.U.T informatique articule des enseignements théoriques et des travaux pratiques. Il permet de développer des connaissances et des compétences techniques spécialisées en informatique.",
+      media: {   
+        type: "IMAGE",
+        source: {
+          url: "https://iutp.univ-poitiers.fr/geii/wp-content/uploads/sites/21/2014/03/Logo-GEII-fond-transparent-HR.gif",
+        }
+      }
+    },
     {
       title: "Décembre 2020 - Juin 2021",
       cardTitle: "DU AccES",
@@ -45,8 +80,28 @@ const Parcours = () => {
   ];
 
   return (
-    <div>
-      <Chrono items={items} mode="VERTICAL_ALTERNATING"/>    
+    <div className="parcours">
+      <Chrono 
+        items={items} 
+        mode="VERTICAL_ALTERNATING"  
+        hideControls
+        enableOutline
+        enableBreakPoint verticalBreakPoint={900}
+        mediaHeight={300}
+        classNames={{
+          title: 'parcours-title',
+          cardTitle: 'parcours-card-title',
+          cardSubTitle: 'parcours-subtitle',
+          cardDetailedText: 'parcours-detailed-text',
+        }}
+        theme={{
+          primary: '#27272E',
+          secondary: '#32363b',
+          cardBgColor: '#32363b',
+          titleColor: 'black',
+          titleColorActive: '#ff014f',
+        }}
+      />    
     </div>
   );
 };
